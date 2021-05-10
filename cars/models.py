8c12de577d0e48c7d1895289325e246340883b38
99Ace <chunliang.ang@gmail.com>
2021-05-10 09:18:41 +0000

    add car model and render data

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CarDeal(models.Model):
+    deal_ref = models.CharField(
+        max_length=10,
+        blank=False
+    )
+    car_plate = models.CharField(
+        max_length=8,
+        blank=False
+    )
+    chassis_no = models.CharField(
+        max_length=25,
+        blank=True,
+        default="none"
+    )
+    engine_no = models.CharField(
+        max_length=20,
+        blank=True,
+        default="none"
+    )
+    current_owner = models.CharField(
+        max_length=30,
+        blank=True,
+        default="none"
+    )
+    buyer = models.CharField(
+        max_length=30,
+        blank=True,
+        default="none"
+    )
+    seller = models.CharField(
+        max_length=30,
+        blank=True,
+        default="none"
+    )
+    referral = models.CharField(
+        max_length=30,
+        blank=True,
+        default="none"
+    )
+
+    def __str__(self):
+        return (self.deal_ref+" ("+self.car_plate+")")
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CarDeal
+from mikar9.models import Invoice
 
 # Create( your views here.
+
+
 def cars(request):
-    return render(request, 'cars.html')
+    data = CarDeal.objects.all().order_by('deal_ref')
+    car = {
+            'deal_ref': 'Not selected',
+            'car_plate': 'Not selected',
+            'chassis_no': 'Not selected',
+            'engine_no': 'Not selected',
+            'buyer': 'Not selected',
+            'seller': 'Not selected',
+            'referral': 'Not selected'
+        }
+    if request.method == "POST":
+        item_id = request.POST.get('carplate')
+        if item_id == 'none':
+            pass
+        else:
+            car = get_object_or_404(CarDeal, pk=item_id)
+    
+    print( car.car_plate )
+    invoices = Invoice.objects.filter(car_plate=car.car_plate)
+
+    return render(request, 'cars.html', {
+        'data': data,
+        'car': car,
+        'invoices': invoices
+    })
